d968229416b6d3dca9636f75afa07f6eff0c1aed
Boyan Qian <boyan.qian@valtech.com>
2022-01-29 11:49:57 +0100

    Initial commit

---
+++ basic/methodsLib.py
@@ -0,0 +1,3 @@
+
+def printLocation():
+    print("Frankfurt")
---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+import basic.methodsLib
+
+print("this is something")
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    basic.methodsLib.printLocation()
+
